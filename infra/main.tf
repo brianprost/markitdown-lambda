ea7e85853df6732a541c36a8bb97f48f93f83b6a
Brian Prost <me@brianprost.com>
2025-04-24 12:18:09 -0400

    Update IAM role config and add Terraform files to gitignore

--- infra/main.tf
+++ infra/main.tf
@@ -19,27 +19,13 @@ module "lambda" {
   timeout     = 10
 }
 
-resource "aws_iam_role" "lambda_role" {
-    name = "${local.app_name}-lambda-role"
-    assume_role_policy = jsonencode({
-        Version   = "2012-10-17",
-        Statement = [{
-            Action    = "sts:AssumeRole",
-            Effect    = "Allow",
-            Principal = {
-                Service = "lambda.amazonaws.com"
-            }
-        }]
-    })
-}
-
 resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
-    role       = aws_iam_role.lambda_role.name
+    role       = module.lambda.lambda_role_name
     policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
 resource "aws_iam_role_policy_attachment" "lambda_s3_readonly" {
-    role       = aws_iam_role.lambda_role.name
+    role       = module.lambda.lambda_role_name
     policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
 }
 
